f2e3d76c306864ac873066ea68004b2f2194480c
krskovape <krskovape@natur.cuni.cz>
2021-11-25 13:57:08 +0100

    Přidání ročních průtoků

--- du2.py
+++ du2.py
@@ -1,12 +1,10 @@
 import csv
-
+#SEDMIDENNÍ PRŮTOKY
 #otevření souboru se vstupními daty a definice výstupních souborů
 with open("vstup.csv", encoding="utf-8") as csvinfile,\
-    open("vystup_7dni.csv", "w", newline="", encoding="utf-8") as csvoutfile_tyden,\
-    open("vystup_rok.csv", "w", newline="", encoding="utf-8") as csvoutfile_rok:
+    open("vystup_7dni.csv", "w", newline="", encoding="utf-8") as csvoutfile_tyden:
     reader = csv.reader(csvinfile, delimiter = ",")
     writer_tyden = csv.writer(csvoutfile_tyden)
-    writer_rok = csv.writer(csvoutfile_rok)
 
     #inicializace proměnných
     pocet_radku = 0   
@@ -40,6 +38,52 @@ with open("vstup.csv", encoding="utf-8") as csvinfile,\
         prumer_prutok = sum_prutok / zbyle_dny
         prvni_den_tyden[5] = f" {prumer_prutok:.4f}"
         writer_tyden.writerow(prvni_den_tyden)
+
+#ROČNÍ PRŮTOKY
+#otevření souboru se vstupními daty a definice výstupních souborů
+with open("vstup.csv", encoding="utf-8") as csvinfile,\
+    open("vystup_rok.csv", "w", newline="", encoding="utf-8") as csvoutfile_rok:
+    reader = csv.reader(csvinfile, delimiter = ",")
+    writer_rok = csv.writer(csvoutfile_rok)
+
+    #inicializace proměnných
+    pocet_radku = 0   
+    sum_prutok = 0
+    zbyle_dny = 0
+    
+    for row in reader:
+        aktualni_rok = int(row[2])
+        #přiřazení roku z prvního řádku do proměnné představující rok, za který se počítá průměr
+        if pocet_radku == 0:
+            rok_prutoku = int(row[2])
+            prvni_den_rok = row
+        
+        #ošetření nekorektního vstupu
+        try:
+            sum_prutok += float(row[5])
+            zbyle_dny += 1
+        except ValueError:
+            pass
+
+        #spočítání ročního průměru a jeho zapsání do souboru
+        if aktualni_rok != rok_prutoku:
+            aktualni_prutok = float(row[5])
+            prumer_prutok = (sum_prutok - aktualni_prutok) / (zbyle_dny - 1)
+            prvni_den_rok[5] = f" {prumer_prutok:.4f}"
+            writer_rok.writerow(prvni_den_rok)
+            sum_prutok = aktualni_prutok
+            prumer_prutok = 0
+            zbyle_dny = 1
+            rok_prutoku = int(row[2])
+            prvni_den_rok = row
+        pocet_radku += 1
+
+    #dopočítání průměru za poslední rok
+    if aktualni_rok == rok_prutoku:
+        prumer_prutok = sum_prutok / zbyle_dny
+        prvni_den_rok[5] = f" {prumer_prutok:.4f}"
+        writer_rok.writerow(prvni_den_rok)
+
 
 
 
